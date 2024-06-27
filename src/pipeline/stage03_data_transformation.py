1a2d84169e98c581b9b093c41302841b0a5f8e6c
MJ Domenzain <47764531+mjdocevedo@users.noreply.github.com>
2024-06-27 16:15:28 +0200

    Added third stage of the pipeline

---
+++ src/pipeline/stage03_data_transformation.py
@@ -0,0 +1,42 @@
+import sys
+from pathlib import Path
+
+# Add parent directory to path
+parent_folder = str(Path(__file__).parent.parent.parent)
+sys.path.append(parent_folder)
+
+from src.config_manager import ConfigurationManager
+from src.data.data_transformation import DataTransformation
+from custom_logger  import logger
+
+STAGE_NAME = "Data Transformation stage"
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        try:
+            with open(Path("data/status.txt"), 'r') as f:
+                status = f.read().split(" ")[-1]
+            
+            if status == "True":
+                config = ConfigurationManager()
+                data_transformation_config = config.get_data_transformation_config()
+                data_transformation = DataTransformation(config = data_transformation_config)
+                data_transformation.train_test_splitting()
+            else:
+                raise Exception("Your data schema is not valid")
+        
+        except Exception as e:
+            print(e)
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+        obj =  DataTransformationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx=======x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
